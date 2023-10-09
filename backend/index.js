4ec8a9c415bfa451dd127d459630882338c34a89
Can Kayalı <volkand135@gmail.com>
2023-10-09 10:25:41 +0300

    HTTP ROUTE

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { response } from 'express';
 import {PORT} from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Hello World!')
+}) 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
 })
