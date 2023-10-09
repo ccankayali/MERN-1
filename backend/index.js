48f60dd8247f391425252908a971a5c8e10774b5
Can Kayalı <volkand135@gmail.com>
2023-10-09 13:26:50 +0300

    MongoDB connect develop

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,49 @@
-import express, { response } from 'express';
-import {PORT, mongoDBURL} from './config.js';
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModels.js'; // Doğru import yolunu kullanmalısınız
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send('Hello World!')
-}) 
+// Middleware
+app.use(express.json());
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Hello World!');
+});
+
+// Route for saving a new book
+app.post('/books', async (request, response) => {
+    try {
+        if (!request.body.title || !request.body.author || !request.body.publishYear) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishyear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.error(error);
+        response.status(500).send('Internal Server Error');
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
         console.log('MongoDB connected');
-        app.listen(PORT,() => {
+        app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     })
     .catch((error) => {
-        console.log(error);
+        console.error(error);
     });
